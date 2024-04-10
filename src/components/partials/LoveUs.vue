5e1dde6de48df367b0b170812032fe32661e3c43
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-04-10 17:28:20 +0200

    [add] LoveUs.vue-Card

--- src/components/partials/LoveUs.vue
+++ src/components/partials/LoveUs.vue
@@ -16,6 +16,43 @@
         </h2>
       </div>
 
+      <div class="d-flex justify-content-between ">
+
+        <div class="card shadow" style="width: 22rem;">
+          <div class="circle-img">
+            <img src="../../assets/images/testimonial-avata-02.jpg" class="card-img-top" alt="...">
+          </div>
+          <div class="card-body text-center">
+            <p class="card-text">I am free to learn at my own place, follow  my own schedule and choose the subject I like. Great study portal for people like me.</p>
+            <p><strong>MADLEY PONDOR</strong></p>
+            <span class="name">/ Freelancer</span>
+          </div>
+        </div>
+
+        <div class="card shadow" style="width: 22rem;">
+          <div class="circle-img">
+            <img src="../../assets/images/testimonial-avata-04.jpg" class="card-img-top" alt="...">
+          </div>
+          <div class="card-body text-center">
+            <p class="card-text">MaxCoach is my best choce. Their tutors are smart and professional when deadling with students.</p>
+            <p><strong>MADLEY PONDOR</strong></p>
+            <span class="name">/ IT Specialist</span>
+          </div>
+        </div>
+
+        <div class="card shadow" style="width: 22rem;">
+          <div class="circle-img">
+            <img src="../../assets/images/testimonial-avata-01.jpg" class="card-img-top" alt="...">
+          </div>
+          <div class="card-body text-center">
+            <p class="card-text">I am happy with their arrangement of lessons and subjects. They reflect a scientific investigation.</p>
+            <p><strong>LUVIC DUBBLE</strong></p>
+            <span class="name">/ Private Tutor</span>
+          </div>
+        </div>
+        
+    </div>
+
     </div>
   </section>
 </template>
@@ -27,6 +64,7 @@
 .back-loveus{
   background-color: $color-ticket;
   .title{
+    margin-bottom: 100px;
       align-items: center;
       p{
         color: $font-bottom;
@@ -42,6 +80,34 @@
         color: $btn-green;
         font-size: 2.8rem;
         font-weight: 300;
+      }
+    }
+    .card{
+      position: relative;
+      border-style: none;
+      border: 1px;
+      padding-left: 8px;
+      padding-right: 8px;
+      .card-body{
+        padding-bottom: 50px;
+      }
+      .circle-img{
+        position: absolute;
+        height: 140px;
+        width: 140px;
+        overflow: hidden;
+        border-radius: 50%;
+        left: 50%;
+        transform: translateY(-50%) translateX(-50%);
+        img {
+          width: 100%;
+        }
+      }
+      .card-text{
+        margin-top: 85px;
+        color: $color-font;
+        margin-bottom: 35px;
+        font-size: 1.1rem;
       }
     }
 }
